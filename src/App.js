0d0ee1ba88a69d2cb7095c0072c7794ccee113bf
life_liu <liulili@ruyigou.com>
2020-09-09 17:12:23 +0800

    test hook

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//单个useState
+// import React,{useState} from 'react';
 
-function App() {
+// export default function App(){
+//   const [count,setCount] = useState(0)
+//   return (
+//     <div>
+//       <p>you clicked {count} times</p>
+//       <button onClick={()=>setCount(count+1)}>click me</button>
+//     </div>
+//   )
+// }
+
+//多个useState
+// import React,{useState} from 'react'
+// function App(){
+//   const [count, setCount]  = useState(0)
+//   const [age, setAge]  = useState(20)
+//   return (
+//     <div className="App">
+//       <p>you clicked {count} times</p>
+//       <button onClick={()=>setCount(count+1)}>add count</button>
+//   <p>you age is {age}</p>
+//   <button onClick={()=>setAge(age-1)}> change age</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+//单个useEffect
+import React,{useState,useEffect} from 'react'
+
+function Counter(){
+  const [count,setCount] = useState(0)
+  const [age,setAge] = useState(20)
+  //相当于componentDidMount 和 componentDidUpdate
+  // useEffect(()=>{
+  //   console.log(`You clicked ${count} times`)
+  // });
+
+  // useEffect(()=>{
+  //   console.log('挂载和状态更新时执行')
+  //   return ()=>{
+  //     console.log('状态更新和卸载组件时执行')
+  //   }
+  // },[count])//只监控count状态
+  useEffect(()=>{
+    console.log('挂载和状态更新时执行')
+    return ()=>{
+      console.log('状态更新和卸载组件时执行')
+    }
+  },[])//只监控count状态
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>you clicked {count} times</p>
+      <button onClick={()=>setCount(count+1)}>add count</button>
+      <p>you age {age} times</p>
+      <button onClick={()=>setAge(age+1)}>add age</button>~
     </div>
-  );
+  )
 }
 
-export default App;
+export default Counter
